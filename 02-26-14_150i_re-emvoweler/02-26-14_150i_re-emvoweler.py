9c0b3151dd12535f5cbd28db5451fbc3837ef930
Patrick Slattery <patrick@pagerduty.com>
2014-03-10 00:14:53 -0700

    refactor #150. not quite there

--- 02-26-14_150i_re-emvoweler/02-26-14_150i_re-emvoweler.py
+++ 02-26-14_150i_re-emvoweler/02-26-14_150i_re-emvoweler.py
@@ -4,30 +4,35 @@ def re_emvowel(consonants, vowels):
     f.close()
 
     vowels = list(vowels)
-    letters = list(consonants)
+    consonants = list(consonants)
     new_str = ''
 
+    # we need to check if any combination of consonants + vowel is a word
+    def has_valid_word(consonants, vowel):
+        i = 0
+        while i < len(consonants) + len(vowel):
+            possible_word = ''.join(consonants[0:i]) + vowel + ''.join(consonants[i:])
+            if possible_word in valid_words:
+                return possible_word
+            i += 1
+        return False
+
     while len(vowels) > 0:
-        i, j = 0, 1
         vowel = vowels.pop(0)
 
-        while i < len(letters):
-            while j < len(letters):
-                sub_cons = letters[i:j]
-                k = 0
-                while k <= len(sub_cons):
-                    possible_word = ''.join(sub_cons[0:k]) + vowel + ''.join(sub_cons[k:])
+        # length of consonants we will attempt to use, go for the largest words first
+        i = len(consonants)
+        while i > 0:
+            word = has_valid_word(consonants[0:i], vowel)
+            if word:
+                new_str += word + ' '
+                consonants = consonants[i:]
+                print(consonants)
+                break
+            i -= 1
 
-                    if possible_word in valid_words:
-                        new_str += possible_word + ' '
-                        letters = letters[j:]
-                        i, j = 0, 1
-                        vowel = vowels.pop(0) if len(vowels) > 0 else ''
-                    k += 1
-
-                j += 1
-            i, j = i + 1, i + 2
     print(new_str)
+
 
 re_emvowel('wwllfndffthstrds', 'eieoeaeoi')
 re_emvowel('bbsrshpdlkftbllsndhvmrbndblbnsthndlts', 'aieaeaeieooaaaeoeeaeoeaau')
